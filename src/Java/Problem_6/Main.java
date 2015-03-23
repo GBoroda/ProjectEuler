8ecce0633d0ef27a2c8b3b7b0f70081ffc36fc2b
GBoroda <MSBoroda@gmail.com>
2015-03-23 13:34:04 +0200

    Tests

--- src/Java/Problem_6/Main.java
+++ src/Java/Problem_6/Main.java
@@ -4,11 +4,8 @@ import static java.lang.Math.*;
 
 public class Main {
     public static void main(String[] args) {
-        int anwer = 0;
-        for (int i = 1; i < 101; i++) {
-            anwer= (int) getDifference(100);
-        }
-        System.out.println(anwer);
+        int answer = (int) getDifference(100);
+        System.out.println(answer);
     }
 
     public static double getDifference(int x) {
--- src/Java/Problem_7/MainTest.java
+++ src/Java/Problem_7/MainTest.java
@@ -1,11 +1,18 @@
 package Problem_7;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainTest {
 
     @Test
     public void testIsSimple() throws Exception {
-
+        long x = 127;
+        Assert.assertTrue(Main.isSimple(x));
+        boolean test = false;
+        for (int i = 2; i < x; i++) {
+            if (x%i==0) test=true;
+        }
+        Assert.assertFalse(test);
     }
 }
